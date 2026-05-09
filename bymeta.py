92fc0fd851b3cc6b65e84960feefcb22bdac5115
rishugupta1 <rishabhitorya@gmail.com>
2026-05-09 13:18:52 +0530

    fixed render port

--- bymeta.py
+++ bymeta.py
@@ -302,4 +302,10 @@ def webhook():
 # =========================================
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000)),
+        debug=False
+    )
